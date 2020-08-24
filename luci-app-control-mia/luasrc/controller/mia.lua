526a528d0c9575612f7a5fecac4b1992fad0fa12
siropboy <64370904+siropboy@users.noreply.github.com>
2020-08-24 11:06:25 +0800

    修复有时启动不了现象

--- luci-app-control-mia/luasrc/controller/mia.lua
+++ luci-app-control-mia/luasrc/controller/mia.lua
@@ -6,13 +6,13 @@ function index()
 	end
 
 	entry({"admin","control"}, firstchild(), "Control", 44).dependent = false
-	entry({"admin","control","mia"},cbi("mia"),_("时间控制"),10).dependent=true
+	entry({"admin","control","mia"},cbi("mia"),_("Internet Access Schedule Control"),10).dependent=true
 	entry({"admin","control","mia","status"},call("status")).leaf=true
 end
 
 function status()
 	local e={}
-	e.status=luci.sys.call("iptables -L FORWARD |grep MIA >/dev/null")==0
+	e.running=luci.sys.call("iptables -L INPUT |grep MIA >/dev/null")==0
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
