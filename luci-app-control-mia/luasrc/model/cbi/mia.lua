3afc0df97f2ee9f611f36e77c32c786a10a23664
siropboy <64370904+siropboy@users.noreply.github.com>
2020-08-25 03:55:32 +0800

    mia acl

--- luci-app-control-mia/luasrc/model/cbi/mia.lua
+++ luci-app-control-mia/luasrc/model/cbi/mia.lua
@@ -1,66 +1,57 @@
+local o = require "luci.sys"
+local a, t, e
+a = Map("mia", translate("Internet Time Control"))
+a.template = "mia/index"
 
-a=Map("mia",translate("Internet Access Schedule Control"),translate("Access Schedule Control Settins"))
-a:section(SimpleSection).template  = "mia/mia_status"
+t = a:section(TypedSection, "basic")
+t.anonymous = true
 
-t=a:section(TypedSection,"basic")
-t.anonymous=true
+e = t:option(DummyValue, "mia_status", translate("Status"))
+e.template = "mia/mia"
+e.value = translate("Collecting data...")
 
-e=t:option(Flag,"enable",translate("Enabled"))
-e.rmempty=false
+e = t:option(Flag, "enable", translate("Enabled"))
+e.rmempty = false
 
-e=t:option(Flag,"strict",translate("Strict Mode"))
-e.rmempty=false
-e.description = translate("Strict Mode will degrade CPU performance, but it can achieve better results")
+t = a:section(TypedSection, "macbind", translate("Client Settings"))
+t.template = "cbi/tblsection"
+t.anonymous = true
+t.addremove = true
 
-t=a:section(TypedSection,"macbind",translate("Client Rules"))
-t.template="cbi/tblsection"
-t.anonymous=true
-t.addremove=true
+e = t:option(Flag, "enable", translate("Enabled"))
+e.rmempty = false
 
-e=t:option(Flag,"enable",translate("√"))
-e.rmempty=false
-e.default="1"
+e = t:option(Value, "macaddr", "MAC")
+e.rmempty = true
+o.net.mac_hints(function(t, a) e:value(t, "%s (%s)" % {t, a}) end)
 
-e=t:option(Value,"macaddr",translate("MAC address (Computer Name)"))
-e.rmempty=true
-luci.sys.net.mac_hints(function(t,a)
-e:value(t,"%s (%s)"%{t,a})
-end)
+e = t:option(Value, "timeon", translate("No Internet start time"))
+e.default = "00:00"
+e.optional = false
 
-e=t:option(Value,"timeon",translate("Start time"))
-e.default="00:00"
-e.optional=false
+e = t:option(Value, "timeoff", translate("No Internet end time"))
+e.default = "23:59"
+e.optional = false
 
-e=t:option(Value,"timeoff",translate("End time"))
-e.default="23:59"
-e.optional=false
+e = t:option(Flag, "z1", translate("Monday"))
+e.rmempty = true
 
-e=t:option(Flag,"z1",translate("Mon"))
-e.rmempty=true
-e.default=1
+e = t:option(Flag, "z2", translate("Tuesday"))
+e.rmempty = true
 
-e=t:option(Flag,"z2",translate("Tue"))
-e.rmempty=true
-e.default=1
+e = t:option(Flag, "z3", translate("Wednesday"))
+e.rmempty = true
 
-e=t:option(Flag,"z3",translate("Wed"))
-e.rmempty=true
-e.default=1
+e = t:option(Flag, "z4", translate("Thursday"))
+e.rmempty = true
 
-e=t:option(Flag,"z4",translate("Thu"))
-e.rmempty=true
-e.default=1
+e = t:option(Flag, "z5", translate("Friday"))
+e.rmempty = true
 
-e=t:option(Flag,"z5",translate("Fri"))
-e.rmempty=true
-e.default=1
+e = t:option(Flag, "z6", translate("Saturday"))
+e.rmempty = true
 
-e=t:option(Flag,"z6",translate("Sat"))
-e.rmempty=true
-e.default=1
-
-e=t:option(Flag,"z7",translate("Sun"))
-e.rmempty=true
-e.default=1
+e = t:option(Flag, "z7", translate("Sunday"))
+e.rmempty = true
 
 return a
